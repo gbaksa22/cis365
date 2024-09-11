f7da9e7ee4a9eee504a75fee62e81983525a0021
gbaksa22 <gbaksa22@gmail.com>
2024-09-11 18:32:42 -0400

    bug fix

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -36,17 +36,16 @@ def dfs_find_goal(graph, start, goal, path=None, visited=None):
         return path  # Goal found, return the path
 
     for neighbor in graph[start]:
-        for node in neighbor:
-            if node not in visited:
-                result = dfs_find_goal(graph, node, goal, path, visited)
-                if result:
-                    return result
+        if neighbor not in visited:
+            result = dfs_find_goal(graph, neighbor, goal, path, visited)
+            if result:
+                return result
 
     path.pop()  # Backtrack
     return None  # Goal not found in this path
 
-path_to_goal = dfs_find_goal(graph, "start", "goal")
-print("Path to goal using DFS:", path_to_goal)
+path_to_goal_dfs = dfs_find_goal(graph, "start", "goal")
+print("Path to goal using DFS:", path_to_goal_dfs)
 
 def bfs_find_goal(graph, start, goal):
     visited = set()
@@ -61,12 +60,11 @@ def bfs_find_goal(graph, start, goal):
             return path  # Goal found, return the path
 
         for neighbor in graph[vertex]:
-            for node in neighbor:
-                if node not in visited:
-                    visited.add(node)
-                    new_path = list(path)
-                    new_path.append(node)
-                    queue.append(new_path)
+            if neighbor not in visited:
+                visited.add(neighbor)
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
 
-path_to_goal = bfs_find_goal(graph, "start", "goal")
-print("Path to goal using BFS:", path_to_goal)
+path_to_goal_bfs = bfs_find_goal(graph, "start", "goal")
+print("Path to goal using BFS:", path_to_goal_bfs)
