15bd28754b9202e6c89a3618c8098cc441dceea3
gbaksa22 <gbaksa22@gmail.com>
2024-11-19 22:27:14 -0500

    import image

--- assignments/assignment13-pca/assignment13.py
+++ assignments/assignment13-pca/assignment13.py
@@ -1,4 +1,15 @@
 # import image
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# Load the image
+image = Image.open('JetSkiAir.jpg').convert('L')  # Convert to grayscale
+myImage = np.asarray(image)  # Convert to NumPy array
+plt.imshow(myImage, cmap='gray')
+plt.title("Original Image")
+plt.show()
+
 
 # 1. Find the mean of the image
 
@@ -17,10 +28,11 @@
 # 8. Create a variable ‘eigenVectorsToKeep’ that only contains that number of eigenVectors
 
 # 9. Create a variable ‘compressedImage’ it should be calculated by using:
-compressedImage = np.matmul(myImageMinusMean, eigenVectorsToKeep)
+# compressedImage = np.matmul(myImageMinusMean, eigenVectorsToKeep)
 
 # 10. Create a lossy uncompressed image from the above variable: 
-lossyUncompressedImage = np.matmul(compressedImage, np.transpose(eigenVectorsToKeep)) + myImageMean
+# lossyUncompressedImage = np.matmul(compressedImage, np.transpose(eigenVectorsToKeep)) + myImageMean
+
 # 11. Output the compressed image
 
 # Repeat the above steps with the variable numberOfEigenVectorsToKeep = {15, 100, 200}
