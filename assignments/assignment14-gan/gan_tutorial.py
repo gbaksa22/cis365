63273df0d963600be753f8e7c2ed53e952b96eea
gbaksa22 <gbaksa22@gmail.com>
2024-11-25 11:20:33 -0500

    1d function

--- assignments/assignment14-gan/gan_tutorial.py
+++ assignments/assignment14-gan/gan_tutorial.py
@@ -1,4 +1,25 @@
 # 1. Select a One-Dimensional Function
+# Import required library
+from matplotlib import pyplot as plt
+
+# Simple function to calculate x^2
+def calculate(x):
+    return x * x
+
+# Define inputs
+inputs = [-0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+# Calculate outputs
+outputs = [calculate(x) for x in inputs]
+
+# Plot the result
+plt.plot(inputs, outputs)
+plt.title("y = x^2 Function")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid(True)  # Optional: Adds a grid for better visualization
+plt.show()
+
 # 2. Define a Discriminator Model 
 # 3. Define a Generator Model 
 # 4. Training the Generator Model 
